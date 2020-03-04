90cce2bf1d20453f39226d6e40cb7c84817a82c1
AbdulrhmanMahfooz <d7oome7722@gmail.com>
2020-03-04 19:47:08 +0200

    Notification page and order page cart button

--- lib/src/screens/Buy.dart
+++ lib/src/screens/Buy.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:drive_thru/src/screens/Orders.dart';
 import 'package:flutter/material.dart';
 import 'package:page_transition/page_transition.dart';
 import './Timerpage.dart';
@@ -547,7 +548,7 @@ class _BuyState extends State<Buy> {
                     'Units' : widget.product_quantity,
                     'Time To Done' : widget.timeToDone
                   }).then((value){
-                    Navigator.pushReplacement(context, PageTransition(type: PageTransitionType.leftToRight, child: TimerPage(timeInMen: widget.timeToDone,)));
+                    Navigator.pushReplacement(context, PageTransition(type: PageTransitionType.leftToRight, child: Orders()));
                   }).catchError((e){print(e);});
                             
                 },
